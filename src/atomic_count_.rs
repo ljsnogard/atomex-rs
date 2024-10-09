6e3a0c5d634a5ff3a6708dac7c6374afb60faed6
lino <lino_snsalias@outlook.com>
2024-10-09 17:14:15 +0800

    Added readme and examples

--- src/atomic_count_.rs
+++ src/atomic_count_.rs
@@ -27,6 +27,24 @@ where
         + fetch::Sub<Value = V>,
     B: BorrowMut<<V as TrAtomicData>::AtomicCell>,
 {
+    /// Create an instance by moving or borrowing an `TrAtomicCell`
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// use core::sync::atomic::*;
+    /// use atomex::AtomicCount;
+    ///
+    /// let atm = AtomicUsize::new(0usize);
+    /// let cnt = AtomicCount::<usize, AtomicUsize>::new(atm);
+    ///
+    /// let mut atm = cnt.into_inner();
+    /// let cnt = AtomicCount::<usize, &mut AtomicUsize>::new(&mut atm);
+    ///
+    /// assert_eq!(cnt.inc(), 0usize);
+    /// assert_eq!(cnt.dec(), 1usize);
+    /// assert_eq!(cnt.val(), 0usize);
+    /// ```
     pub const fn new(cell: B) -> Self {
         AtomicCount(cell, PhantomData)
     }
@@ -51,6 +69,18 @@ where
 
     pub fn val(&self) -> V {
         self.0.borrow().load(Ordering::Relaxed)
+    }
+}
+
+impl<V> AtomicCount<V, <V as TrAtomicData>::AtomicCell>
+where
+    V: TrAtomicData + funty::Integral,
+    <V as TrAtomicData>::AtomicCell: TrAtomicCell<Value = V>
+        + fetch::Add<Value = V>
+        + fetch::Sub<Value = V>,
+{
+    pub fn into_inner(self) -> <V as TrAtomicData>::AtomicCell {
+        self.0
     }
 }
 
